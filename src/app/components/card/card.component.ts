780f41d850f829f0585530d98166f1e131e1abf1
aymen2782 <aymen.sellaouti@gmail.com>
2021-04-05 13:59:55 +0100

    card exercice

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  name = 'aymen sellaouti';
+  job = 'teacher';
+  path = 'rotating_card_profile3.png';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
