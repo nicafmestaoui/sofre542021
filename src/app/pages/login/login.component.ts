1229bbeef64b70207e28d39084efe438c55e25f8
aymen2782 <aymen.sellaouti@gmail.com>
2021-04-08 12:18:04 +0100

    login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private toaster: ToastrService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login(credentials: { email: string; password: string }): void {
+    this.authService.login(credentials).subscribe(
+      (data) => {
+        localStorage.setItem('token', data.id);
+        this.toaster.success('Bienvenu');
+        this.router.navigate(['cv']);
+      },
+      (erreur) => {
+        this.toaster.error('Veuillez vérifier vos credentials');
+      }
+    );
   }
-
 }
